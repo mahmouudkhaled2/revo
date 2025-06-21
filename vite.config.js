3ec91d1c3d715cd141065d57bcbbafa6ffaaabf3
mahmouudKhalled <foktop018@gmail.com>
2025-06-21 07:54:05 +0300

    Add connect-history-api-fallback dependency and configure Vite server middleware for history API support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import history from 'connect-history-api-fallback';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
+    middlewareMode: true,
+  },
+  configureServer: (server) => {
+    server.middlewares.use(history());
   },
 });
+
