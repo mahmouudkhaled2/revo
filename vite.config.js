275ab33b2d36ea3b326e7a4aa3f4bb7359d7f179
mahmouudKhalled <foktop018@gmail.com>
2025-06-22 03:10:00 +0300

    Enable middleware mode in Vite server configuration to support custom middleware functionality.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@
   export default defineConfig({
     plugins: [react()],
     server: {
-      // middlewareMode: true,
+      middlewareMode: true,
     },
     configureServer: (server) => {
       server.middlewares.use(history());
